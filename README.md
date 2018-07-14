d9b125619261646663a3e01bf38b7948661956e3
Chris Morgan <crmorgan@users.noreply.github.com>
2018-07-14 12:05:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# nservicebus-intro
+Introduction to NServiceBus retail demo
+
+
+
+https://github.com/dotnet/templating/wiki/Available-templates-for-dotnet-new
+dotnet new -i "ParticularTemplates::*"
+
---
+++ src/store-web/Controllers/CheckoutController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace store_web.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+			TempData["OrderId"] = GenerateOrderId();
+
+            return View();
+        }
+
+		public IActionResult PlaceOrder(int orderId)
+		{
+			TempData["OrderId"] = orderId;
+
+			// TODO: send a PlaceOrder command to the bus
+
+			return View("Confirmation");
+		}
+
+		public IActionResult Confirmation()
+		{
+			return View();
+		}
+
+
+		private int GenerateOrderId()
+		{
+			var random = new Random();
+			return random.Next(1, 1000);
+		}
+	}
+}
